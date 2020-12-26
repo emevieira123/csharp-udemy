d02c4a928892ba9bf0f904f6b08804d55de9f749
Emerson Vieira <emevieira@gmail.com>
2020-12-26 13:09:25 -0400

    Criacao do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace listas_genericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

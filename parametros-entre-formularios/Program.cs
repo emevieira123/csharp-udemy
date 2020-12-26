4220503d5de013b6a2bce2e0f3288e9f938b9500
Emerson Vieira <emevieira@gmail.com>
2020-12-26 14:38:13 -0400

    Parametros entre formularios

---
+++ parametros-entre-formularios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace parametros_entre_formularios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
